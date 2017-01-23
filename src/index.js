8f36e19d42d152059b9387867e507383eec642d9
Joshua Wiens <joshuaw@easymetrics.com>
2017-01-23 01:28:07 -0600

    feat(sum): add demo modules and tests

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import sum from './sum';
+
+const log = console.log;
+
+const dbConfig = {
+  server: 'localhost',
+  isOnline: true
+};
+
+log(`Results: ${sum(1, 2)}`);
+
+const dbCon = new Promise((resolve, reject) => {
+  log(`Try connecting to database: ${dbConfig.server}`);
+  dbConfig.isOnline
+    ? resolve('online')
+    : reject('offline');
+}).then(data => log(`Promise resolved: ${data}`)).catch(err => log(`Promise rejected: ${err.toString()}`));
+
+log(`Promise Info: ${dbCon}`);
